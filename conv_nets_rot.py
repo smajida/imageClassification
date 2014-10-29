9fa1bc1d83e913c2de97e4374d466cf116c52ade
bengioe <bengioe@gmail.com>
2014-10-29 16:48:28 -0400

    conv nets with rotations + numpy MLP

--- conv_nets_rot.py
+++ conv_nets_rot.py
@@ -1,10 +1,13 @@
+import time
 import numpy as np
+import scipy.misc
 
 import theano
 import theano.tensor as T
 from theano.tensor.nnet import conv
 from theano.tensor.signal import downsample
 
+theano.config.floatX = 'float32'
 
 def make_hidden_layer(x, nin, nout, activation):
     print (nin,nout),(nout,)
@@ -22,7 +25,7 @@ def make_conv_layer(input, filter_shape, image_shape, poolsize=(3, 3)):
                np.prod(poolsize))
 
     k = np.sqrt(6./(fan_in+fan_out))
-    W = theano.shared(np.random.uniform(-k, k, size=filter_shape),name='Wconv',borrow=True)
+    W = theano.shared(np.float32(np.random.uniform(-k, k, size=filter_shape)),name='Wconv',borrow=True)
     
     # bias on individual filters
     b_values = np.zeros((filter_shape[0],), dtype=theano.config.floatX)
@@ -51,38 +54,48 @@ def make_conv_layer(input, filter_shape, image_shape, poolsize=(3, 3)):
 
 class ConvNet:
     def __init__(self,
-                 batch_size = 50,
-                 nfilters = 10,
-                 sizeA = 13,
-                 sizeB = 7,
+                 batch_size = 200,
+                 nfilters = [40,40,200],
+                 sizeA = 7,
+                 sizeB = 5,
+                 sizeC = 5,
                  nhidden = 100,
-                 gamma = 1e-4):
+                 gamma = 1e-6,
+                 imgW = 48):
         self.batch_size = batch_size
         x = T.matrix()
         y = T.matrix()
         lr = T.scalar()
         
-        img_x = x.reshape((batch_size,1,48,48))
+        img_x = x.reshape((batch_size,1,imgW,imgW))
         # 
-        c_p, c = make_conv_layer(img_x, [nfilters,1,sizeA,sizeA], [batch_size, 1, 48, 48],(2,2))
-        new_size = (48-sizeA+1)/2
+        c_p, c = make_conv_layer(img_x, [nfilters[0],1,sizeA,sizeA], 
+                                 [batch_size, 1, imgW, imgW],
+                                 (2,2))
+        new_size = (imgW-sizeA+1)/2
         #
-        if 1:
-            d_p, d = make_conv_layer(c, [nfilters,nfilters,sizeB,sizeB], 
-                                     [batch_size, nfilters, new_size, new_size],(2,2))
-            new_size = (new_size-sizeB+1)/2
-            
-        flat_size = nfilters * new_size**2
+        d_p, d = make_conv_layer(c, [nfilters[1],nfilters[0],sizeB,sizeB], 
+                                 [batch_size, nfilters[0], new_size, new_size],
+                                 (2,2))
+        new_size = (new_size-sizeB+1)/2
+        #
+        e_p, e = make_conv_layer(d, [nfilters[2],nfilters[1],sizeC,sizeC], 
+                                 [batch_size, nfilters[1], new_size, new_size],
+                                 (2,2))
+        new_size = (new_size-sizeC+1)/2
+
+        flat_size = nfilters[-1] * new_size**2
         # 
-        h_p, h = make_hidden_layer(d.reshape((batch_size,flat_size)),
+
+        f = e.reshape((batch_size,flat_size))
+        h_p, h = make_hidden_layer(f,
                                    flat_size, nhidden, T.tanh)
         #
-        if 0:
-            i_p, i = make_hidden_layer(h, nhidden, nhidden, T.tanh)
-        o_p, o = make_hidden_layer(h, nhidden, 10, T.nnet.softmax)
+        o_p, o = make_hidden_layer(h, nhidden,
+                                   10, T.nnet.softmax)
 
         
-        params = c_p + h_p + o_p + d_p# + i_p
+        params = c_p + o_p + d_p + e_p + h_p 
         print params
         L2 = sum([T.sum(i**2) for i in params if 'W' in i.name])
         
@@ -97,32 +110,48 @@ class ConvNet:
         self.evaluate = theano.function([x],
                                         o)
         
-    def fit(self,data, step_test, testfunc, nepochs=2000, tau = 80):
+    def fit(self,data, step_test, testfunc, nepochs=800, tau = 70):
         X, Y = data
-        lr = 0.01
+        lr = 0.1
 
         nbatches = X.shape[0] / self.batch_size
 
         print "training."
 
+        def rot(X):
+            return np.float32([1./255*scipy.misc.imrotate(
+                i.reshape((48,48)),np.random.uniform(0,360)).flatten() for i in X])
+
+        range_len_X = range(len(X))
         # train for a while
         for i in range(nepochs):
             c = 0
+            t0 = time.time()
             for batch in range(nbatches):
                 x = X[batch*self.batch_size:(batch+1)*self.batch_size]
-                x += np.float32(np.random.uniform(-4./255,4./255,x.shape))
+                #x = X[np.random.choice(range_len_X,self.batch_size)]
+                x = rot(x)
+                #x += np.float32(np.random.uniform(-4./255,4./255,x.shape))
                 c += self.learn(x,
                                 Y[batch*self.batch_size:(batch+1)*self.batch_size],
                                 lr * tau / (i * 1. + tau))
-            print i,c
+            print i,c,time.time()-t0
             if i%step_test == 0:
-                testfunc()
+                testfunc(i)
+
     def test(self, data):
         x,Y = data
         y = self.eval(x)
         cerr = 1-1.*np.equal(np.argmax(y,axis=1),(np.argmax(Y,axis=1))).sum() / Y.shape[0]
         # return mean square error and classification error
         return ((Y-y)**2).sum() / y.shape[0], cerr
+
+    def test_rot(self, data):
+        x,Y = data
+        y = self.eval_rot(x)
+        cerr = 1-1.*np.equal(np.argmax(y,axis=1),(np.argmax(Y,axis=1))).sum() / Y.shape[0]
+        # return mean square error and classification error
+        return ((Y-y)**2).sum() / y.shape[0], cerr, y
 
     def eval(self, x):
         y_i = []
@@ -131,8 +160,22 @@ class ConvNet:
         y = np.vstack(y_i)
         return y
 
+
+    def eval_rot(self, x):
+        def rot(X,r):
+            return np.float32([1./255*scipy.misc.imrotate(
+                i.reshape((48,48)),r).flatten() for i in X])
+        y_i = []
+        for k in range(x.shape[0] / self.batch_size):
+            y_k = []
+            for irot in range(0,360,36):
+                y_k.append(self.evaluate(rot(x[k*self.batch_size:(k+1)*self.batch_size],irot)))
+            y_i.append(np.float32(y_k).mean(axis=0))
+        y = np.vstack(y_i)
+        return y
+
 def onehot(a,maxv):
-    z = np.zeros((a.shape[0],maxv))
+    z = np.zeros((a.shape[0],maxv),dtype='float32')
     z[range(a.shape[0]),a.flatten()] = 1
     return z
 
@@ -155,13 +198,55 @@ def main():
 
         net = ConvNet()
         def showerr():
-            print net.test([trainX,trainY]), net.test([testX, testY])
+            print net.test([trainX,trainY]), net.test([testX, testY]), net.test_rot([testX, testY])
         net.fit([trainX,trainY], 5, showerr)
         print "End of train for fold",k
         print net.test([testX, testY])
 
 
+def main_test():
+    n_folds = 5
+    
+    all_X = np.float32(np.load('train_inputs.npy') / 255.)
+    all_Y = np.load('train_outputs.npy').reshape((all_X.shape[0],1))
+    test_X = np.float32(np.load('test_inputs.npy') / 255.)
+    
+    n_examples = all_X.shape[0]
+    fs = foldsize = n_examples / n_folds
+    # k folds
+    for k in range(n_folds):
+        trainX = np.vstack([all_X[:k*fs],all_X[(k+1)*fs:]])#[:1000]
+        trainY = onehot(np.vstack([all_Y[:k*fs],all_Y[(k+1)*fs:]]), 10)#[:1000]
+        testX = all_X[k*fs:(k+1)*fs]#[:1000]
+        testY = onehot(all_Y[k*fs:(k+1)*fs], 10)#[:1000]
+        print trainX.shape,trainY.shape,testX.shape,testY.shape
+        
+
+        net = ConvNet()
+        best_err = [1]
+        def showerr(epoch):
+            q = (net.test([trainX,trainY]), net.test([testX, testY]), net.test_rot([testX, testY]))
+            print q[0],q[1],(q[2][0],q[2][1])
+            file('log.txt','a').write(str("["+str(time.time()))+"] "+str((q[0],q[1],(q[2][0],q[2][1])))+"\n")
+            if q[2][1] < best_err[0]:
+                best_err[0] = q[2][1]
+                y = net.eval_rot(test_X)
+                f = file('test_output_40_40_200_755_100_%dep_%.4f.csv'%(epoch,100*best_err[0]),'w')
+                f.write("Id,Prediction\n")
+                for i,p in enumerate(y):
+                    f.write("%d,%d\n"%(i+1,np.argmax(p)))
+                f.close()
+        net.fit([trainX,trainY], 10, showerr)
+        print "End of train for fold",k
+        print net.test([testX, testY])
+        #####
+        break
+
+
 def test():
+    def q(X):
+        return np.array([scipy.misc.imresize(i.reshape((48,48)),(28,28)).flatten() for i in X])
+
     all_X = np.float32(np.load('train_inputs.npy') / 255.)
     all_Y = onehot(np.load('train_outputs.npy').reshape((all_X.shape[0],1)), 10)
     test_X = np.float32(np.load('test_inputs.npy') / 255.)#[:1000]
@@ -171,7 +256,7 @@ def test():
     def showerr():
         print net.test([all_X,all_Y])
         y = net.eval(test_X)
-        f = file('test_output.csv','w')
+        f = file('test_output_40_13.3_5.2_400.csv','w')
         f.write("Id,Prediction\n")
         for i,p in enumerate(y):
             f.write("%d,%d\n"%(i+1,np.argmax(p)))
@@ -181,5 +266,6 @@ def test():
     
 
 if __name__=="__main__":
-    main()
+    main_test()
+    #main()
     #test()
